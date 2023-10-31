2801acda02950825981cebe669de387a6ac47b61
User <User@DESKTOP-Q9QV65T>
2023-10-31 22:54:10 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Static
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student("Ənvər Zöhrabov", "P101", 21);
+            Console.WriteLine(student1);
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static
+{
+    internal class Student
+    {
+        public static int nextId = 1;
+        public int ID {  get; set; }
+        public string Fullname { get; set; }
+        public string GroupNo { get; set; }
+        public int Age { get; set; }
+        public Student(string fullname, string groupNo, int age)
+        {
+            ID = nextId++;
+            Fullname = fullname;
+            GroupNo = ValidateGroupNo(groupNo);
+            Age = age;
+        }
+
+        private string ValidateGroupNo(string groupNo)
+        {
+            if (groupNo.Length == 4 && char.IsUpper(groupNo[0]) && int.TryParse(groupNo.Substring(1), out int result))
+            {
+                return groupNo;
+            }
+            else
+            {
+                 new ArgumentException("Group number must consist of one uppercase letter followed by three digits (e.g., P101).");
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"ID: {Id}, Fullname: {Fullname}, GroupNo: {GroupNo}, Age: {Age}";
+        }
+    }
+
+   
+}
+
